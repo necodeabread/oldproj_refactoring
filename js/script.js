a231c64c993480925e9708f677bbfb59d78f2949
Hlib Drobinko <xlebywekmail@gmail.com>
2021-11-03 13:40:23 +0200

    small refactor for the rest sections, added simple script for slides, changed some incorrectly formatted svgs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const notificationToggler = document.querySelector('.promo-notifications-text');
     const pointer = document.querySelector('.promo-notifications-pointer');
 
-    notificationToggler.addEventListener('click', function() {
-        if(pointer.classList.contains('inactive')) {
-            pointer.classList.remove('inactive');
-            pointer.classList.add('active');
-            return;
-        } else {
-            pointer.classList.add('inactive');
-            pointer.classList.remove('active');
-        }
+    function toggleClass(toggler, item) {
+        toggler.addEventListener('click', function() {
+            if(item.classList.contains('inactive')) {
+                item.classList.remove('inactive');
+                item.classList.add('active');
+                return;
+            } else {
+                item.classList.add('inactive');
+                item.classList.remove('active');
+            }
+        });
+    }
+
+    toggleClass(notificationToggler, pointer);
+
+    const slides = document.querySelectorAll('.learning__slider-card');
+    slides.forEach(function(slide) {
+        slide.addEventListener('mouseenter', function(e) {   
+            e.target.style.zIndex = 10;
+        });
+        slide.addEventListener('mouseleave', function(e) {
+            e.target.style.zIndex--;
+        });
     });
 }); 
