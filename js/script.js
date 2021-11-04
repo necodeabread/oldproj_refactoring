7925405e024a89231f0145b15e172feea6e63f78
Hlib Drobinko <xlebywekmail@gmail.com>
2021-11-04 16:24:55 +0200

    added simple responsive for main containter type, added some mobile elements and functionality

--- js/script.js
+++ js/script.js
@@ -27,4 +27,25 @@ document.addEventListener('DOMContentLoaded', function() {
             e.target.style.zIndex--;
         });
     });
+
+    function checkScreenWidth() {
+        if(window.screen.width < 768) {
+            const pointerParent = document.querySelector('.promo-notifications');
+            const pointerBg = document.querySelector('.promo-notifications-pointer');
+            pointerBg.parentElement.removeChild(pointerBg);
+            const pointerText = document.querySelector('.promo-notifications-text');
+            pointerText.parentElement.removeChild(pointerText);
+
+            const pointer = document.createElement('button');
+            pointer.innerText = "Get notified when enrollment opens again.";
+            pointer.classList.add('promo-block-main-button');
+            pointerParent.append(pointer);
+            pointer.addEventListener('touchend', function() {
+                pointer.style.background = 'linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)';
+                pointer.innerText = "Done!"
+            });
+        }
+    }
+
+    checkScreenWidth();
 }); 
