ea6bcaa5a0129b59831217bc0e8b83a9964fcb02
Hlib Drobinko <xlebywekmail@gmail.com>
2021-11-07 16:07:12 +0200

    added phpmailer and script for it, moved everything to src dir, fixed some responsive issues, added pageup.scss and a js file for future implementations, added modal window, style and initial script for it

--- src/js/script.js
+++ src/js/script.js
@@ -40,12 +40,56 @@ document.addEventListener('DOMContentLoaded', function() {
             pointer.innerText = "Get notified when enrollment opens again.";
             pointer.classList.add('promo-block-main-button');
             pointerParent.append(pointer);
+
             pointer.addEventListener('touchend', function() {
                 pointer.style.background = 'linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)';
-                pointer.innerText = "Done!"
+                pointer.innerText = "Done!";
             });
-        }
+
+            const triggerButton = document.querySelector('.menu__button_join');
+            triggerButton.addEventListener('touchstart', function() {
+                const overlay = document.querySelector('.overlay');
+                overlay.classList.add('overlay_active');
+                $('body').on('touchstart', function(e) {
+                    if(!$(e.target).closest('.modal').length && !$(e.target).is('.modal') && !$(e.target).is(triggerButton)) {
+                        $('.overlay').fadeOut('slow');
+                    }
+                });
+            });
+        } 
     }
 
     checkScreenWidth();
+
+
+
+    function send(event, php) {
+        console.log('request being sent');
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        var req = new XMLHttpRequest();
+        req.open('POST', php, true);
+        req.onload = function() {
+            if(req.status >= 200 && req.status < 400) {
+                json.parse(this.response); // ie 11
+                console.log(json);
+
+                if(json.result == "success") {
+                    responseModal.classList.add('active');
+                    responseModal.innerText = "Success!";
+                } else {
+                    responseModal.classList.add('active');
+                    responseModal.innerText = "Error";
+                }
+            } else {
+                responseModal.classList.add('active');
+                responseModal.innerText = `Server Error. Number: ${+req.status}`;
+            }
+        }
+        req.onerror = function() {
+            responseModal.classList.add('active');
+            responseModal.innerText = "Request send error";
+        }
+        req.send(new FormData(event.target)); 
+    }
+    
 }); 
